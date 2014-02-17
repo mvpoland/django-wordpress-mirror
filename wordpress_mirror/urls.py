45059f4a2a6f83a4279b5c10d052a1b5cbef48ec
Jan Fabry <jan.fabry@mobilevikings.com>
2014-02-17 10:43:32 +0100

    Make it a 1-on-1 mirror, pass all params to WP

--- wordpress_mirror/urls.py
+++ wordpress_mirror/urls.py
@@ -3,8 +3,6 @@ from django.conf.urls import patterns, url
 from wordpress_mirror.views import overview, permalink, detail
 
 urlpatterns = patterns('',
-    url(r'^$', overview, name='wordpress_blog_overview'),
-    url(r'^page/(?P<page>\d*)/$', overview, name='wordpress_blog_overview'),
-    url(r'^permalink/(?P<post_id>\d*)/$', permalink, name='wordpress_blog_permalink'),
-    url(r'^detail/(?P<slug>.*)/$', detail, name='wordpress_blog_detail'),
+    url(r'^$', mirror, name='wordpress_blog_mirror'),
+    url(r'^(?P<wp_path>.*)$', mirror, name='wordpress_blog_mirror_path'),
 )
