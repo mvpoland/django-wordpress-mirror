27772e8ef5f79a202e0506b5518992db454787cb
San Gillis <gillis.san@gmail.com>
2014-02-18 13:36:04 +0100

    switch to new freyja

--- wordpress_mirror/views.py
+++ wordpress_mirror/views.py
@@ -10,11 +10,12 @@ from django.http import Http404
 from urlparse import urlparse, urlunparse, urljoin
 from django.utils.http import urlencode
 
-from freyja.core.localeurl.utils import locale_path, strip_path
+from freyja.core.localeurl.languages import sitelanguage
 
 import requests
 
 REQUEST_TIMEOUT_SEC = 10
+
 
 class Post(object):
     """
@@ -56,7 +57,7 @@ class Post(object):
         if not self._url_localized:
             if self.data:
                 url_parsed = list(urlparse(self.data['url']))
-                url_parsed[2] = locale_path(url_parsed[2], self.country, self.lang)
+                url_parsed[2] = sitelanguage().locale_url(url_parsed[2], self.lang)
                 self._url_localized = urlunparse(url_parsed)
         return self._url_localized
 
@@ -65,7 +66,7 @@ class Post(object):
         if not self._url_unlocalized:
             if self.data:
                 url_parsed = list(urlparse(self.data['url']))
-                url_parsed[2] = strip_path(url_parsed[2])[2]
+                url_parsed[2] = sitelanguage().path_without_locale(url_parsed[2])
                 self._url_unlocalized = urlunparse(url_parsed)
         return self._url_unlocalized
 
@@ -81,6 +82,7 @@ class Post(object):
                     except ValueError:
                         pass
             return custom_images
+
 
 def get_posts(wp_path='/', wp_query=None, lang=None, country=None):
     # TODO: Caching
@@ -102,6 +104,7 @@ def get_posts(wp_path='/', wp_query=None, lang=None, country=None):
     if 'posts' in json_data:
         json_data['posts'] = [Post(pd, lang, country) for pd in json_data['posts']]
     return json_data
+
 
 def mirror(request, wp_path='/'):
     #namespace = resolve(request.path).namespace
@@ -127,7 +130,8 @@ def mirror(request, wp_path='/'):
                 cur_params['page'] = cur_page + 1
                 context['older_posts_page'] = request.path + '?' + cur_params.urlencode()
 
-        return render_to_response(mapping[site_id]['templates'] + template,
-                context,
-                context_instance=RequestContext(request))
+        return render_to_response(
+            mapping[site_id]['templates'] + template,
+            context,
+            context_instance=RequestContext(request))
     raise Http404
