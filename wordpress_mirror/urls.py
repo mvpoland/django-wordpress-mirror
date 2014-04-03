b6cd2dbd9ed260609b54e44e62cf11354a0eb140
Daniel Borzęcki <borzecki.daniel@gmail.com>
2014-04-03 14:11:44 +0200

    added category view to fetch posts with given category slug

--- wordpress_mirror/urls.py
+++ wordpress_mirror/urls.py
@@ -4,6 +4,7 @@ from wordpress_mirror.views import *
 
 urlpatterns = patterns('',
     url(r'^$', overview, name='wordpress_blog_overview'),
+    url(r'^category/(?P<slug>.*)/$', category, name='wordpress_blog_category'),
     url(r'^page/(?P<page>\d*)/$', overview, name='wordpress_blog_overview'),
     url(r'^permalink/(?P<post_id>\d*)/$', permalink, name='wordpress_blog_permalink'),
     url(r'^detail/(?P<slug>.*)/$', detail, name='wordpress_blog_detail'),
--- wordpress_mirror/views.py
+++ wordpress_mirror/views.py
@@ -9,7 +9,7 @@ from django.template.context import RequestContext
 import requests
 
 
-def overview(request, page='1', count='4'):
+def overview(request, page='1', count='4', slug=''):
     """
     Blog overview
     """
@@ -20,8 +20,9 @@ def overview(request, page='1', count='4'):
     if page == '1':
         count = str(int(count) + 1)
 
-    r = requests.get("%s?json=1&count=%s&page=%s" %
-                     (mapping[site_id]['host'], count, page))
+    category = 'get_category_posts' if slug else '1'
+    r = requests.get("%s?json=%s&count=%s&page=%s&slug=%s" %
+                     (mapping[site_id]['host'], category, count, page, slug))
     json_data = json.loads(r.content)
     # Change the date to a python datetime.datetime object
     for post in json_data['posts']:
@@ -31,11 +32,23 @@ def overview(request, page='1', count='4'):
     context = {'posts': json_data['posts'],
                'max_pages': unicode(json_data['pages']),
                'page': page,
+               'slug': slug,
                }
+
+    # Get optional category title
+    if 'category' in json_data:
+        context['title'] = json_data['category']['title']
 
     return render_to_response(mapping[site_id]['templates'] + 'overview.html',
                               context,
                               context_instance=RequestContext(request))
+
+
+def category(request, slug):
+    """
+    View posts from given category
+    """
+    return overview(request, slug=slug)
 
 
 def permalink(request, post_id):
